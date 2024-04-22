1a3fd29362b028b3b0258423d0e0a616a474f484
Naveen0910 <125568964+Naveen0910@users.noreply.github.com>
2024-04-22 11:57:46 +0530

    Image Uploader : Cloudinary image uploader widget

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -29,8 +29,10 @@ import {
   transformationTypes,
 } from "@/constants";
 import { CustomField } from "./CustomFeild";
-import { useState } from "react";
-import { AspectRatioKey } from "@/lib/utils";
+import { startTransition, useState } from "react";
+import { AspectRatioKey, debounce, deepMergeObjects } from "@/lib/utils";
+import { updateCredits } from "@/lib/actions/user.actions";
+import MediaUploader from "./MediaUploader";
 
 export const formSchema = z.object({
   title: z.string(),
@@ -82,17 +84,45 @@ const TransformationForm = ({
     value: string,
     onChangeField: (value: string) => void
   ) => {
-    const imageSize
+    const imageSize = aspectRatioOptions[value as AspectRatioKey];
+    setImage((prevState: any) => ({
+      ...prevState,
+      aspectRatio: imageSize.aspectRatio,
+      width: imageSize.width,
+      height: imageSize.height,
+    }));
+    setNewTransformation(transformationType.config);
+    return onChangeField(value);
   };
 
   const onInputChangeHandler = (
-    feildName: string,
+    fieldName: string,
     value: string,
     type: string,
     onChangeField: (value: string) => void
-  ) => {};
+  ) => {
+    debounce(() => {
+      setNewTransformation((prevState: any) => ({
+        ...prevState,
+        [type]: {
+          ...prevState?.[type],
+          [fieldName === "prompt" ? "prompt" : "to"]: value,
+        },
+      }));
+    }, 1000);
+  };
 
-  const onTransformHandler = () => {};
+  const onTransformHandler = () => {
+    setIsTransforming(true);
+    setTransformationConfig(
+      deepMergeObjects(newTransformation, transformationConfig)
+    );
+    setNewTransformation(null);
+
+    startTransition(async () => {
+      // await updateCredits(userId,creditFee)
+    });
+  };
 
   return (
     <Form {...form}>
@@ -178,6 +208,22 @@ const TransformationForm = ({
             )}
           </div>
         )}
+        <div className="media-uploader-field">
+          <CustomField
+            control={form.control}
+            name="publicId"
+            className="flex size-full flex-col"
+            render={({ field }) => (
+              <MediaUploader
+                onValueChange={field.onChange}
+                setImage={setImage}
+                publicId={field.value}
+                image={image}
+                type={type}
+              />
+            )}
+          />
+        </div>
         <div className="flex flex-col gap-4">
           <Button
             disabled={isTransforming || newTransformation === null}
